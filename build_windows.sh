99692a23bda98bfb6e3f53582f3756fc8782cc3e
qwertysam <qwertysam@users.noreply.github.com>
2019-08-02 18:38:14 -0400

    Updated for Qt Creator

--- build_windows.sh
+++ build_windows.sh
@@ -1,1 +1,1 @@
-./cmake_mingw.sh . -DURHO3D_64BIT=1 -DMINGW_PREFIX=/usr/bin/x86_64-w64-mingw32 -DURHO3D_HOME=~/Urho3D-1.7.1-mingw/
+./cmake_mingw.sh . -D URHO3D_64BIT=1 -D MINGW_PREFIX=/usr/bin/x86_64-w64-mingw32 -D URHO3D_HOME=~/Urho3D-1.7.1-mingw/
